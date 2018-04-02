222255bdbda88f3bf02894c24a905680ba2a1060
Fabián Camp <fabian.camp@yahoo.com>
2018-04-02 13:32:48 -0500

    ##

---
+++ ProyectoSegundoParcial.py
@@ -0,0 +1,17 @@
+def menuPrincipal():
+    print(" ==============================")
+    print("|Menu Proyecto Segundo Parcial |")
+    print(" ============================== ")
+    print("| 1. Busqueda de numeros en    |")
+    print("|     un conjunto de datos     |")
+    print("| 2. Algoritmo BFS y DFS       |")
+    print("| 3. Salir                     |")
+    print(" ============================== ")
+
+def main():
+    menuPrincipal()
+    a = int(input("Seleccione una opcion: "))
+    if a == 1:
+        menu
+
+main()
