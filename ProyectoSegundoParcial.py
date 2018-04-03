53cd610232ac11bceba19e6657f12fbce9890b77
AdrianML333 <adrianml333@gmail.com>
2018-04-02 22:28:21 -0500

    Busqueda de Numero a 1/2
    Busqueda de numero casi completo

--- ProyectoSegundoParcial.py
+++ ProyectoSegundoParcial.py
@@ -37,26 +37,84 @@ def grafos():
     elif opcionGrafos == 3:
         return
 
-def algortimoBFS():
+#def algortimoBFS():
 
 
 def menuNumeros():
     #Lectura del txt
-    leeArchivo()
-    #Determinar si esta ordenado o no
-    revisaLista()
-    #Pedir numero al usuario
-    #Escoger algoritmo, (ordenado-binary / desordenado-a escoger)
-    #Mostrar dato buscado, numero de operaciones elementales, numero de casilla donde se ubico
+    x = leeArchivo()
+    #Pedir numero al usuario y buscarlo / Algoritmo, (ordenado-binary / desordenado-a escoger)
+    buscaNumero(x)
+    #Salir o volver al menu principal
+    menuFinal()
+
+def buscaNumero(listaNum):
+    num = int(input("Numero a buscar: "))
+    if revisaLista(listaNum) == True:
+        #Binary Search
+        binarySearch(listaNum, num)
+    else:
+        #Sequential Search
+        sequentialSearch(listaNum,num)
+
+
+def menuFinal():
+    while (True):
+        print("1.- Si")
+        print("2.- No")
+        opcionFinal = int(input("Deseas volver al menu principal?: "))
+        if opcionFinal == 1:
+            main()
+        elif opcionFinal == 2:
+            print("Gracias por su preferencia, vuelva pronto :)")
+            break
+        else:
+            print("No es una opcion valida")
+        
+
+def sequentialSearch(lista, num):
+    i = 0
+    while i < len(lista):
+        if lista[i] == num:
+            print("El numero " + num + " se encontro en la posicion " + i +
+             " y se realizaron " + i + " pasos elementales, (comparaciones).")
+        else:
+            i +=1
+
+def binarySearch(lista, num):
+    primer = 0
+    ultimo = len(lista) - 1
+    count = 0
+    while primer <= ultimo == True:
+        i = (primer + ultimo) // 2
+        if lista[i] == num == True:
+            print ("El numero " + num + " se encontro en la posicion " + i +
+                  " y se realizaron " + count + " pasos elementales, (comparaciones).")
+        elif lista[i] > num == True:
+            count +=1
+            ultimo = i - 1
+        elif lista[i] < num == True:
+            count +=1
+            primer = i + 1
+        else:
+            print("El numero a buscar no se encuentra en la lista")
 
 def leeArchivo():
-    lista = open("Datos.txt","r") as ListaNum
-    reader = csv.reader(ListaNum)
-    for line in reader:
-       ListaNum.append(line)
+    listaNum = []
+    with open("Datos.txt") as Lista:
+        reader = csv.reader(Lista)
+        for line in reader:
+            listaNum.append(line)
+    return listaNum
 
-#def revisaLista(lista):
-#
+def revisaLista(lista):
+    x = sorted(lista)
+    if x == lista:
+        print("La lista esta ordenada")
+        return True
+    else:
+        print("La lista no esta ordenada")
+        return False
 
 def main():
     while(True):
